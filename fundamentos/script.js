085edeb00cba3c211a9020576b0ce44e9018d2e5
Daniygor1408 <danieleygor14@gmail.com>
2024-08-29 21:36:23 -0300

    Add files via upload

---
+++ fundamentos/script.js
@@ -0,0 +1,7 @@
+console.log("Olá, mundo!");
+/*let preco = prompt("digite um preco:");
+document.write(preco);*/
+
+let name = prompt("digite seu nome:");
+document.write("ola", nome,".seja bem-vindx ao site!");
+//ola, fulana. seja bem-vindx ao site!
