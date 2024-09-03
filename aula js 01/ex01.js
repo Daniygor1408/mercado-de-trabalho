f08e9b12f7213a6683185388faf3bf8febb33654
Daniygor1408 <danieleygor14@gmail.com>
2024-09-03 19:40:39 -0300

    Add files via upload

---
+++ aula js 01/ex01.js
@@ -0,0 +1,10 @@
+/*Declare duas variaveis chamadas "idade" e "peso",
+//atrbua valores a elas e imprima os valores no console*/
+
+let idade= 40
+console.log (idade)
+let peso= 54
+console.log ("ola , voce tem "+idade+" anos e o seu peso e de "+peso+ "kg")
+//ola, voce tem 40 anos e o seu peso e de 54 kg
+
+
